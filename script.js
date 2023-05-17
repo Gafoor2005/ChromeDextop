b87581b2905291f4d5af451055bdace1215a7253
Gafoor <gafoorma686@gmail.com>
2023-05-17 17:36:00 +0530

    added scroll snap:)

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let mid = document.querySelector(".middle")
 let main = document.querySelector("main")
 let apps = document.querySelector(".apps")
+let container2 = document.querySelector(".container2")
+let topdiv = document.querySelector(".top")
 
+let container2Top = container2.offsetTop;
+let appsMaxScroll = apps.scrollTop;
 let currentPosition = 0
+let gotMax = false;
 // main.scrollTo(0,apps.offsetTop)
-let p1 = 0;
-let p2 = 0;
-main.addEventListener("scroll",()=>{
+let ps = 0;
+let cs = 0;
+let uwList = false;
+let dwList = false;
+main.addEventListener("scroll",async ()=>{
     // text slide in --------
     if(main.scrollTop > (mid.offsetTop - window.innerHeight/1.4)){
         mid.classList.add("active")
@@ -42,5 +49,58 @@ main.addEventListener("scroll",()=>{
     //     currentPosition = 0;
     // }
     // p1 = p2;
+    
+    cs = main.scrollTop;
+    if((cs>container2Top)){
+        if((cs - ps > 0)){
+            if(uwList){
+                return;
+            }
+            if( (apps.scrollTop < appsMaxScroll) && (gotMax)){
+                apps.scrollBy(0,cs - ps)
+                uwList = true;
+                uwList = await new Promise((reslove,reject)=>{
+                    setTimeout(() => {
+                        reslove(false)
+                    }, 500);
+                })
+            }
+            if (!gotMax){
+                apps.scrollBy(0,cs - ps)
+                uwList = true;
+                uwList = await new Promise((reslove,reject)=>{
+                    setTimeout(() => {
+                        reslove(false)
+                    }, 500);
+                })
+                if(appsMaxScroll == apps.scrollTop){
+                    gotMax = true;
+                    // console.log("mashallah");
+                }
+                else{
+                    appsMaxScroll = apps.scrollTop;
+                }
+            }
+        }
+        
+    }
+    if((cs - ps < 0)){
+        if(dwList){
+            return;
+        }
+        if (apps.scrollTop>0){
+            apps.scrollBy(0,cs-ps)
+            dwList = true;
+            dwList = await new Promise((reslove,reject)=>{
+                setTimeout(() => {
+                    reslove(false)
+                }, 500);
+            })
+            // console.log("subhanallah");
+        }
+    }
+
+    ps = cs;
 })
 
+apps.style.scrollBehavior = "smooth";
